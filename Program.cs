cb4b01406852f9e9f6f7b0ee86b48ba1d20a5c58
Andresjales <60202369+Andresjales@users.noreply.github.com>
2020-02-25 09:24:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de x: ");
+            double x = double.Parse(Console.ReadLine());
+            double sumatoria = 0, e = 0;
+            int n = 100;
+
+            for (int i = 0; i < n; i++)
+            {
+                sumatoria += (Math.Pow(x, i)) / Factorial(i);
+                e = sumatoria;
+                Console.WriteLine("\nIteración: " + (i+1));
+                Console.WriteLine("e^x: " + e);
+            }
+        }
+
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            return valor * Factorial(valor - 1);
+        }
+    }
+}
